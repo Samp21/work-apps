87ce9f1910a8025cdf3f81f39fe38941ab9f5f55
Sam Power <samp25@gmail.com>
2023-01-09 15:28:31 +0000

    basic app structure

--- django_file_uploader/image_uploader_app/models.py
+++ django_file_uploader/image_uploader_app/models.py
@@ -1,3 +1,9 @@
+# myapp/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    file = models.FileField(upload_to=None)
+    title = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
--- django_file_uploader/image_uploader_app/views.py
+++ django_file_uploader/image_uploader_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# myapp/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import DocumentForm
+
+
+def upload_document(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('documents')
+    else:
+        form = DocumentForm()
+    return render(request, 'upload_document.html', {'form': form})
